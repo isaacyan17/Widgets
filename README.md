c5dfd8ff3ffcffdd5d7941bf14f9c59c784f3e83
严劲强 <1030442858@qq.com>
2016-12-23 14:29:08 +0800

    android 引导页

--- README.md
+++ README.md
@@ -3,5 +3,15 @@
 
 #### Python脚本替换Android资源(包名,图片,文件内容)
 
-* package： Python4Android
+* 详见： [Python4Android](./app/src/main/java/com/jinqiang/py4android/Python4Android.md)
+
+---
+
+#### Android引导页动画(RxJava)
+
+> 最近在考虑用新的形式构建APP的欢迎页和引导页，发现一个不错的过渡动画，主要是图片的灰度和放大效果,进入后延迟一秒开始动画。
+使用RxJava的Timer操作符实现延迟。
+
+* 详见：[WelcomeActivity](./app/src/main/java/com/jinqiang/welcomebanner/WelcomeActivity.java)
+
 
--- app/src/main/java/com/jinqiang/welcomebanner/WelcomeActivity.java
+++ app/src/main/java/com/jinqiang/welcomebanner/WelcomeActivity.java
@@ -1,22 +1,85 @@
 package com.jinqiang.welcomebanner;
 
+import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.jinqiang.widgets.R;
+
+import java.util.concurrent.TimeUnit;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
 
 /**
  * @author jingqiang
  * @desc TODO
  */
-public class WelcomeActivity extends AppCompatActivity{
+public class WelcomeActivity extends Activity{
+
+    @Bind(R.id.welcome_img)
+    ImageView mImg;
+    //这里可以是图片集
+    private int[] imgs = {
+            R.mipmap.welcome_img
+    };
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome_layout);
+        ButterKnife.bind(this);
+        mImg.setImageResource(imgs[0]);
+        //timer操作符延迟1秒后执行动画。
+        Observable.timer(1000, TimeUnit.MILLISECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action1<Long>() {
+                    @Override
+                    public void call(Long aLong) {
+                        startAnimation();
+                    }
+                });
+    }
 
+    private void startAnimation() {
+        //ofFloat(T target, Property<T, Float> property, float... values)
+        ObjectAnimator animatorX = ObjectAnimator.ofFloat(mImg,"scaleX",1f, 1.25f);
+        ObjectAnimator animatorY = ObjectAnimator.ofFloat(mImg,"scaleY",1f, 1.25f);
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.setDuration(1500)
+                    .play(animatorX)
+                    .with(animatorY);
+        animatorSet.start();
+        animatorSet.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                Toast.makeText(WelcomeActivity.this,"Done",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
     }
 
 
