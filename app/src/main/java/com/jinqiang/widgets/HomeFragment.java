9402a4de978f8945de1dcf8b22c400e526329cc8
严劲强 <1030442858@qq.com>
2017-02-17 11:28:19 +0800

    嵌入React-native

--- app/src/main/java/com/jinqiang/widgets/HomeFragment.java
+++ app/src/main/java/com/jinqiang/widgets/HomeFragment.java
@@ -1,18 +1,38 @@
 package com.jinqiang.widgets;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.app.Fragment;
+import android.widget.Button;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 
 public class HomeFragment extends Fragment {
+    @Bind(R.id.click)
+    Button  btn;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.recycler_main, container, false);
+        View view = inflater.inflate(R.layout.react_layout_demo, container, false);
         return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ButterKnife.bind(this,view);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(),ReactActivity.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/jinqiang/widgets/ReactApplication.java
@@ -0,0 +1,16 @@
+package com.jinqiang.widgets;
+
+import android.app.Application;
+
+import com.facebook.react.ReactNativeHost;
+
+/**
+ * Created by jingqiang on 2017/2/16.
+ */
+
+public class ReactApplication extends Application implements com.facebook.react.ReactApplication{
+    @Override
+    public ReactNativeHost getReactNativeHost() {
+        return null;
+    }
+}
